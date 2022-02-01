a5a1d3d78f515212941dba97058e99faedfa949d
Shriram <ramshri14@gmail.com>
2022-02-01 16:39:05 +0530

    Initial commit

---
+++ posts/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app= express();
+
+// parsing files changes done
+
+
+app.use(bodyParser.json());
+
+const posts={};
+
+
+app.get('/posts',(req,res)=>{
+    res.send(posts);
+});
+
+app.post('/posts',(req,res)=>{
+    const id=randomBytes(4).toString('Hex');
+    const {title}=req.body;
+    
+    posts[id]={
+        id,title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+// changing port values
+
+app.listen(3000,()=>{
+ console.log("listening in port 3000")
+});

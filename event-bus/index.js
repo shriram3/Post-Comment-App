3558133941cf3e87db2d4ac539b8f4fc3585c47e
Shriram <ramshri14@gmail.com>
2022-02-04 23:19:26 +0530

    Error resolved in event listener

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,17 @@ const axios = require ("axios")
 const app=express();
 app.use(bodyParser.json());
 
-app.post('./events',(req,res)=>{
+app.post('/events',(req,res)=>{
     const event = req.body;
-
+    console.log("events is called");
+    console.log(req.body);
     axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
+    axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4002/events', event).catch((err) => {
+    axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: 'OK' });
--- posts/index.js
+++ posts/index.js
@@ -25,14 +25,16 @@ app.post('/posts',async (req,res)=>{
         id,title
     };
 
+    console.log("call to event listener");
     await axios.post('http://localhost:4005/events',{
         type : "PostCreated",
         data : {
             id, title
         }
-    }).catch((err) => {
-        console.log(err.message);
-      });  ;
+    })
+    .catch((err) => {
+        console.log("Error message:"+err.message);
+    }); 
 
     
     res.status(201).send(posts[id]);

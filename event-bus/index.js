3bbfd757c320920dfb2b73a379016275a3c98d73
Shriram <ramshri14@gmail.com>
2022-02-05 14:20:40 +0530

    Storing event-bus to store missed event

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,10 +5,18 @@ const axios = require ("axios")
 const app=express();
 app.use(bodyParser.json());
 
+// Store all the events details
+const Events = [];
+
+
 app.post('/events',(req,res)=>{
     const event = req.body;
     console.log("events is called");
     console.log(req.body);
+
+	Events.push(event);
+
+
     axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
   });
@@ -21,9 +29,13 @@ app.post('/events',(req,res)=>{
   	axios.post('http://localhost:4003/events', event).catch((err) => {
     console.log(err.message);
   });
-  
+
   res.send({ status: 'OK' });
 
+});
+
+app.get('/events',(req,res)=>{
+	res.send(Events);
 });
 
 
